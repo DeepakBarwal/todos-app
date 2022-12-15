85d68c0f51bcd43a0459effca4b01b18b1b28a34
DeepakBarwal <DeepakBarwal98@gmail.com>
2022-12-15 20:07:02 +0530

    added Main, CerateTask and TaskList components. Started working on CreateTask

---
+++ src/components/CreateTask.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+class CreateTask extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            task: '',
+        }
+    };
+    handleChange = (e) => {
+        this.setState({ task: e.target.value });
+    };
+    handleSubmit = e => {
+        e.preventDefault();
+        this.props.createTeak(this.state.task);
+        this.setState({ task: '' });
+    };
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <input type='text' placeholder="Enter task" onChange={this.handleChange} value={this.state.task} autoFocus />
+                <button type="submit">Add</button>
+            </form>
+        );
+    };
+}
+
+export default CreateTask;
