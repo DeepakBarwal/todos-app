ca5918e7d1c2f92ce73b9793868c4743fdea4cc4
DeepakBarwal <DeepakBarwal98@gmail.com>
2022-12-15 22:20:01 +0530

    saving tasks to localStorage

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,7 +2,7 @@ import React from "react";
 import CreateTask from "./CreateTask";
 import TaskList from "./TaskList";
 
-const tasks = [];
+const tasks = localStorage.getItem('tasks') ? JSON.parse(localStorage.getItem('tasks')) : [];
 class Main extends React.Component {
     constructor(props) {
         super(props);
@@ -17,20 +17,24 @@ class Main extends React.Component {
         }
         tasks.push({ task, isCompleted: false });
         this.setState({ tasks });
+        localStorage.setItem('tasks', JSON.stringify(tasks));
     };
     toggleTask = (taskId) => {
         const taskItem = tasks[taskId];
         taskItem.isCompleted = !taskItem.isCompleted;
         this.setState({ tasks });
+        localStorage.setItem('tasks', JSON.stringify(tasks));
     };
     deleteTask = (taskId) => {
         tasks.splice(taskId, 1);
         this.setState({ tasks });
+        localStorage.setItem('tasks', JSON.stringify(tasks));
     };
     editTask = (taskId, task) => {
         const taskItem = tasks[taskId];
         taskItem.task = task;
         this.setState({ tasks });
+        localStorage.setItem('tasks', JSON.stringify(tasks));
     };
     render() {
         return (
